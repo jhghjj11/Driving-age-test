c0f34d4c362a493af53f08ef05bd9bffb8a4bb1e
jhghjj11 <57930789+jhghjj11@users.noreply.github.com>
2020-07-26 19:47:14 +0800

    Add files via upload

---
+++ Driving age test.py
@@ -0,0 +1,13 @@
+nation = input('Where you from:')
+age = input('What is your age:')
+age = int(age)
+if nation == 'Taiwan' :
+    if age >= 18:
+        print('You are allow to drive with liences')
+    else:
+        print('Not for you')
+elif nation =='USA':
+    if age >= 16:
+        print('You are allow to drive with liences')
+    else:
+        print('Not for you')
